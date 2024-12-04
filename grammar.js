4c3e32ddcf1e4c519da933ed9611322f420442cd
Paulo Hernandez <paulovitormsh@hotmail.com>
2024-12-03 23:10:23 -0300

    fix: restructure grammar to have eol on the separator nodes

--- grammar.js
+++ grammar.js
@@ -15,34 +15,41 @@ module.exports = grammar({
     source_file: $ => optional(
       choice(
         $._body,
-        seq($._body, $.conflict),
-        seq($._body, $.comment),
+        alias($._conflict_no_eol, $.conflict),
+        alias($._comment_no_eol, $.comment),
+        seq($._body, alias($._conflict_no_eol, $.conflict)),
+        seq($._body, alias($._comment_no_eol, $.comment)),
       ),
     ),
     _body:       $ => repeat1(
       choice(
         alias($._eol, $.comment),
-        seq($.conflict, $._eol),
-        seq($.comment, $._eol),
+        $.conflict,
+        $.comment,
       ),
     ),
-    conflict:    $ => seq(
-      $.start, $._eol,
+    _conflict_no_end: $ => seq(
+      $.start,
       optional(field("ours", alias($._body, $.hunk))),
-      optional(seq(
-        $.common_sep, $._eol,
-        optional(field("common", alias($._body, $.hunk)))
-      )),
-      $.sep, $._eol,
+      optional(
+        seq(
+          $.common_sep,
+          optional(field("common", alias($._body, $.hunk)))
+        ),
+      ),
+      $.sep,
       optional(field("theirs", alias($._body, $.hunk))),
-      $.end,
     ),
-    start:       $ => prec.right(1, seq(/[<]{7,}[ \t]*/, optional(field("label", alias(/[^\r\n]+/, $.label))))),
-    common_sep:  $ => prec.right(1, seq(/[|]{7,}[ \t]*/, optional(field("label", alias(/[^\r\n]+/, $.label))))),
-    sep:         $ => prec.right(1, seq(/[=]{7,}[ \t]*/, optional(field("label", alias(/[^\r\n]+/, $.label))))),
-    end:         $ => prec.right(1, seq(/[>]{7,}[ \t]*/, optional(field("label", alias(/[^\r\n]+/, $.label))))),
-    _eol:        $ => /[\r\n]|\r\n/,
-    comment:     $ => repeat1($._char),
+    conflict:    $ => seq($._conflict_no_end, $.end),
+    _conflict_no_eol: $ => seq($._conflict_no_end, alias($._end_no_eol, $.end)),
+    start:       $ => prec.right(1, seq(/[<]{7,}[ \t]*/, optional(field("label", alias(/[^\r\n]+/, $.label))), $._eol)),
+    common_sep:  $ => prec.right(1, seq(/[|]{7,}[ \t]*/, optional(field("label", alias(/[^\r\n]+/, $.label))), $._eol)),
+    sep:         $ => prec.right(1, seq(/[=]{7,}[ \t]*/, optional(field("label", alias(/[^\r\n]+/, $.label))), $._eol)),
+    end:         $ => prec.right(1, seq($._end_no_eol, $._eol)),
+    _end_no_eol: $ => prec.right(1, seq(/[>]{7,}[ \t]*/, optional(field("label", alias(/[^\r\n]+/, $.label))))),
+    comment:     $ => seq($._comment_no_eol, $._eol),
+    _comment_no_eol: $ => repeat1($._char),
     _char:       $ => /[^\r\n]/,
+    _eol:        $ => /[\r\n]|\r\n/,
   }
 });
